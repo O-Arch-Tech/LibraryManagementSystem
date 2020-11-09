86c3f9ba4e2919de499d8bddf233df771653cea7
Kai22 <kid22500@gmail.com>
2020-11-09 19:15:12 +0200

    Fixed Book.java

--- src/librarymanagementsystem/Main.java
+++ src/librarymanagementsystem/Main.java
@@ -1,5 +1,11 @@
 package librarymanagementsystem;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Khalid Tarek
@@ -7,8 +13,19 @@ package librarymanagementsystem;
 public class Main {
 
     public static void main() {
-        // TODO code application logic here
-        System.out.println("Hello World!");
+        // TODO code application logic here        
+        Date harryPotterDate = null;
+        try {
+            harryPotterDate = new SimpleDateFormat("yyyy-MM-dd").parse("1997-06-26");
+        } catch (ParseException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        Book harryPotter1 = new Book(111000, 2, 
+                "Harry Potter and the Sorcerer's Stone", 
+                "J.K. Rowling", harryPotterDate);
+        
+        System.out.println(harryPotter1);
     }
     
 }
