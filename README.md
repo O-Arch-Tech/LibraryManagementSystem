96c85ac9e71d2fc5635fe5f8ea811e0c1e83e6aa
Kai22 <kid22500@gmail.com>
2020-11-09 18:10:11 +0200

    V0.02 Testing

---
+++ README.md
@@ -0,0 +1,2 @@
+# LibraryManagmentSystem
+A simple java library system that was made by The RAD Squad for Software Engineering (2) Final Project
--- src/librarymanagementsystem/Main.java
+++ src/librarymanagementsystem/Main.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package librarymanagementsystem;
 
 /**
  *
- * @author Kai22
+ * @author Khalid Tarek
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+    public static void main() {
         // TODO code application logic here
+        System.out.println("Hello World!");
     }
     
 }
